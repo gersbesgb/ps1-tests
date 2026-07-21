278c16635f02a08cfe10dadb78da030ec6735623
gersbesgb <gersbesgb@gmail.com>
2026-07-21 19:38:22 +0200

    Enhance output colors and formatting in script

--- linkconvo.ps1
+++ linkconvo.ps1
@@ -1,17 +1,19 @@
 Clear-Host
-
+Write-Host "=======================================================" -ForegroundColor Red
 Write-Host " [ GersBes ] " -ForegroundColor Red -NoNewline
-Write-Host "-> GitHub Link Converter v1.0.0" -ForegroundColor Cyan
-Write-Host "=======================================================" -ForegroundColor DarkGray
+Write-Host "-> GitHub Link Converter v1.0.0" -ForegroundColor Red
+Write-Host "=======================================================" -ForegroundColor Red
 
 $userInput = Read-Host "Paste everything after github.com/"
-Write-Host "Example: gersbesgb/GersBes-Download-Checker/blob/main/GersBes-Download-Checker.ps1" -ForegroundColor Yellow
+!Write-Host "Example: gersbesgb/GersBes-Download-Checker/blob/main/GersBes-Download-Checker.ps1" -ForegroundColor Purple
+
+
 $rawPath = $userInput -replace "/blob/", "/"
 $fullRawLink = "https://raw.githubusercontent.com/$rawPath"
 
 Write-Host "`n===============================================================================================" -ForegroundColor Purple
-# 3. Output the full PowerShell web cradle
+
 $command = "powershell -ExecutionPolicy Bypass -Command `"Invoke-Expression (Invoke-RestMethod '$fullRawLink')`""
 
-Write-Host $command -ForegroundColor Purple
+Write-Host $command -ForegroundColor Green
 Write-Host "===============================================================================================" -ForegroundColor Purple
