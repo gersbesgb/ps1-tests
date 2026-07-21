dac38bc528d49f243b8f396ea859e18cddfd6698
gersbesgb <gersbesgb@gmail.com>
2026-07-21 19:44:20 +0200

    Change color scheme in linkconvo.ps1 output

--- linkconvo.ps1
+++ linkconvo.ps1
@@ -1,19 +1,18 @@
 Clear-Host
-Write-Host "=======================================================" -ForegroundColor Red
+
 Write-Host " [ GersBes ] " -ForegroundColor Red -NoNewline
-Write-Host "-> GitHub Link Converter v1.0.0" -ForegroundColor Red
-Write-Host "=======================================================" -ForegroundColor Red
+Write-Host "-> GitHub Link Converter v1.0.0" -ForegroundColor DarkMagenta
+Write-Host "=======================================================" -ForegroundColor DarkMagenta
 
+# User Input
 $userInput = Read-Host "Paste everything after github.com/"
-!Write-Host "Example: gersbesgb/GersBes-Download-Checker/blob/main/GersBes-Download-Checker.ps1" -ForegroundColor Purple
-
+Write-Host "Example: gersbesgb/GersBes-Download-Checker/blob/main/GersBes-Download-Checker.ps1" -ForegroundColor Yellow
 
 $rawPath = $userInput -replace "/blob/", "/"
 $fullRawLink = "https://raw.githubusercontent.com/$rawPath"
 
-Write-Host "`n===============================================================================================" -ForegroundColor Purple
-
 $command = "powershell -ExecutionPolicy Bypass -Command `"Invoke-Expression (Invoke-RestMethod '$fullRawLink')`""
 
-Write-Host $command -ForegroundColor Purple
-Write-Host "===============================================================================================" -ForegroundColor Purple
+Write-Host "`n===============================================================================================" -ForegroundColor DarkMagenta
+Write-Host $command -ForegroundColor DarkMagenta
+Write-Host "===============================================================================================" -ForegroundColor DarkMagenta
